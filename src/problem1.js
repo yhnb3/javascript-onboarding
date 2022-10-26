d652a80dc86c28003b7680f0df82a254536bf1df
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-26 15:32:36 +0900

    Feat : solve problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,28 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!possiblePage(pobi) || !possiblePage(crong)) return -1;
+
+  const pobiScore = Math.max(makeScore(pobi[0]), makeScore(pobi[1]));
+  const crongScore = Math.max(makeScore(crong[0]), makeScore(crong[1]));
+
+  if (pobiScore > crongScore) return 1;
+  if (pobiScore < crongScore) return 2;
+  return 0;
+}
+
+function possiblePage([page1, page2]) {
+  if (page2 - page1 !== 1) return false;
+  if (!(page1 & 1)) return false;
+  if (Math.max(page1, page2) > 400) return false;
+  if (Math.min(page1, page2) < 1) return false;
+  return true;
+}
+
+function makeScore(number) {
+  const numToArr = String(number).split("");
+  return Math.max(
+    numToArr.reduce((acc, cur) => acc + Number(cur), 0),
+    numToArr.reduce((acc, cur) => acc * Number(cur), 1)
+  );
 }
 
 module.exports = problem1;
