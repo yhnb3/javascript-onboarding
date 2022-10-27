91f6a92ac4e191303f06e575f845d043e6e95e9f
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-27 15:46:21 +0900

    Feat(Problem3): make countClap fnc & solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const numbers = Array(number)
+    .fill(0)
+    .map((_, idx) => idx + 1);
+  return numbers.reduce((acc, cur) => acc + countClap(cur), 0);
+}
+
+function countClap(number) {
+  const [matchString] = String(number).match(/[369]+/g) || [""];
+  return matchString.length;
 }
 
 module.exports = problem3;
