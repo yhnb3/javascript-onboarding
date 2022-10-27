e70b3fa913a55fa516940d032c8bb0686be4cd1f
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-28 00:32:49 +0900

    Feat(Problem6) : make makeRegString & makeReg & checkDuplicatedNickname fnc

--- src/problem6.js
+++ src/problem6.js
@@ -8,4 +8,23 @@ function validateEmail(email) {
   return email.match(/@email.com/);
 }
 
+function checkDupicatedNickname(nickname1, nickname2) {
+  const regString = makeRegString(nickname2);
+  const regExp = makeReg(regString);
+  return !!nickname1.match(regExp);
+}
+
+function makeRegString(word) {
+  const wordArr = word.split("");
+  const doubleWordArr = wordArr.map((alpha, idx) => {
+    if (idx === wordArr.findLastIndex()) return "";
+    return `(${alpha}${wordArr[idx + 1]})?`;
+  });
+  return doubleWordArr.join("");
+}
+
+function makeReg(regString) {
+  return new RegExp(regString);
+}
+
 module.exports = problem6;
