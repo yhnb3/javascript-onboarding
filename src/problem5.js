6ca68991ae765f912a26b6a7b5e53e70d14c6984
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-28 00:04:36 +0900

    Feat(Problem6) : make validateEmail fnc

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,10 @@
 function problem5(money) {
   const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const answer = moneyUnit.map((unit) => {
+  return moneyUnit.map((unit) => {
     const need = Math.floor(money / unit);
     money %= unit;
     return need;
   });
-  return answer;
 }
 
 module.exports = problem5;
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
 function problem6(forms) {
+  const emailSet = new Set();
   var answer;
   return answer;
+}
+
+function validateEmail(email) {
+  return email.match(/@email.com/);
 }
 
 module.exports = problem6;
