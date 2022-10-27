2317c141e3fbb48fb60dac6442b4c8dce69f9385
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-27 14:20:36 +0900

    Feat(Problem2) : add deleteDuplication fnc

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (cryptogram.length === 1) return cryptogram;
+
+  let compareString;
+  const alphaSet = new Set(cryptogram.split(""));
+
+  do {
+    compareString = cryptogram;
+    cryptogram = deleteDuplication(compareString, alphaSet);
+  } while (cryptogram && compareString !== cryptogram);
+
+  return cryptogram;
+}
+
+function deleteDuplication(string, alphaSet) {
+  alphaSet.forEach((alpha) => {
+    string = string.replace(alpha + alpha, "");
+  });
+  return string;
 }
 
 module.exports = problem2;
