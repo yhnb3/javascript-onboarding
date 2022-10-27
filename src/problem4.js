3f7f8947de53319c03b7020577ec4521898d9407
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-27 19:10:09 +0900

    Feat(Problem4): make converAlphabet fnc

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const wordArr = word.split("");
+  const convertedWordArr = wordArr.map((alphabet) => convertAlphabet(alphabet));
+  return convertedWordArr.join("");
+}
+
+function convertAlphabet(alphabet) {
+  if (!alphabet.match(/[A-Za-z]/)) return alphabet;
+  const isUpper = alphabet.toUpperCase() === alphabet;
+  const greenFrogCode = isUpper
+    ? "Z".charCodeAt() + "A".charCodeAt() - alphabet.charCodeAt()
+    : "z".charCodeAt() + "a".charCodeAt() - alphabet.charCodeAt();
+  return String.fromCharCode(greenFrogCode);
 }
 
 module.exports = problem4;
