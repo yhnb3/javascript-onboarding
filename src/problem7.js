cac44f73836aa6e68a4066631b1df82a64ebbad2
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-28 15:49:59 +0900

    Feat(Problem7) : add sortFriend fnc

--- src/problem7.js
+++ src/problem7.js
@@ -3,8 +3,11 @@ function problem7(user, friends, visitors) {
   const friendsScore = makeFriendScore(user, friendsGraph);
   addVisitScore(friendsScore, visitors);
   addNearFriendScore(friendsGraph, friendsScore);
-  const validFriend = filterValidFriend(friendsScore);
-
+  const validFriends = filterValidFriend(friendsScore);
+  const sortedFriends = validFriends.sort((a, b) =>
+    sortFriend(a, b, friendsScore)
+  );
+  console.log(sortedFriends);
   return ["andole", "jun", "bedi"];
 }
 
@@ -20,26 +23,28 @@ function makeFriendGraph(friends, user) {
 }
 
 function makeFriendScore(user, friendsGraph) {
+  const userFriendsSet = friendsGraph[user];
   const userArr = Object.keys(friendsGraph);
   const friendScore = {};
   userArr.forEach((name) => {
+    if (name === "USER") return;
     if (name === user) return;
+    if (userFriendsSet.has(name)) return;
     friendScore[name] = 0;
   });
   return friendScore;
 }
 
-function addVisitScore(friendScore, visit) {
-  visit.forEach((name) => {
+function addVisitScore(friendScore, visitors) {
+  visitors.forEach((name) => {
     if (!friendScore[name]) friendScore[name] = 0;
     friendScore[name] += 1;
   });
 }
 
 function addNearFriendScore(friendsGraph, friendScore) {
-  const user = friendsGraph["USER"];
-  const userFriendsArr = [...friendsGraph[user]];
-  userFriendsArr.ForEach((name) => {
+  const friendsArr = Object.keys(friendScore);
+  friendsArr.forEach((name) => {
     if (isNearFriend(name, friendsGraph)) {
       friendScore[name] += 10;
     }
@@ -47,6 +52,8 @@ function addNearFriendScore(friendsGraph, friendScore) {
 }
 
 function isNearFriend(name, friendsGraph) {
+  if (!friendsGraph[name]) return false;
+
   const user = friendsGraph["USER"];
   const userFriendsSet = friendsGraph[user];
   const targetUserFriendsArr = [...friendsGraph[name]];
@@ -56,6 +63,11 @@ function isNearFriend(name, friendsGraph) {
 function filterValidFriend(friendScore) {
   const friendsArr = Object.keys(friendScore);
   return friendsArr.filter((name) => friendScore[name] > 0);
+}
+
+function sortFriend(a, b, friendScore) {
+  if (friendScore[a] === friendScore[b]) return b - a;
+  return friendScore[b] - friendScore[a];
 }
 
 module.exports = problem7;
