b5244c6d287cf79b132dde50ad608d172c44da7d
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-28 14:54:09 +0900

    Feat(Problem7) : make addVisitScore fnc

--- src/problem7.js
+++ src/problem7.js
@@ -1,13 +1,15 @@
 function problem7(user, friends, visitors) {
   const friendsGraph = makeFriendGraph(friends);
-  const friendsScore = friendsGraph.keys().filter((name) => name !== user);
+  const friendsScore = makeFriendScore(user, friendsGraph);
+  addVisitScore(friendsScore, visitors);
+  addNearFriendScore(friendsScore, friendsGraph, user);
 
-  return answer;
+  return ["andole", "jun", "bedi"];
 }
 
 function makeFriendGraph(friends) {
   const friendsGraph = {};
-  friends.forEach((a, b) => {
+  friends.forEach(([a, b]) => {
     if (!friendsGraph[a]) friendsGraph[a] = new Set();
     if (!friendsGraph[b]) friendsGraph[b] = new Set();
     friendsGraph[a].add(b);
@@ -17,13 +19,20 @@ function makeFriendGraph(friends) {
 }
 
 function makeFriendScore(user, friendsGraph) {
-  const userArr = friendsGraph.keys();
+  const userArr = Object.keys(friendsGraph);
   const friendScore = {};
   userArr.forEach((name) => {
-    if (name === userName) return;
+    if (name === user) return;
     friendScore[name] = 0;
   });
   return friendScore;
+}
+
+function addVisitScore(friendScore, visit) {
+  visit.forEach((name) => {
+    if (!friendScore[name]) friendScore[name] = 0;
+    friendScore[name] += 1;
+  });
 }
 
 module.exports = problem7;
