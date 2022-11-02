dff3da8efc72194d4482015e17982d4ff65ff77f
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-02 18:17:02 +0900

    Feat(Problem1) : fix possiblePage -> impossiblePage

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,5 @@
 function problem1(pobi, crong) {
-  if (!possiblePage(pobi) || !possiblePage(crong)) return -1;
+  if (impossiblePage(pobi) || impossiblePage(crong)) return -1;
 
   const pobiScore = Math.max(makeScore(pobi[0]), makeScore(pobi[1]));
   const crongScore = Math.max(makeScore(crong[0]), makeScore(crong[1]));
@@ -9,13 +9,13 @@ function problem1(pobi, crong) {
   return 0;
 }
 
-function possiblePage(pages) {
+function impossiblePage(pages) {
   const [page1, page2] = pages;
-  if (page2 - page1 !== 1) return false;
-  if (!(page1 & 1)) return false;
-  if (Math.max(page1, page2) > 400) return false;
-  if (Math.min(page1, page2) < 1) return false;
-  return true;
+  if (page2 - page1 !== 1) return true;
+  if (!(page1 & 1)) return true;
+  if (Math.max(page1, page2) > 400) return true;
+  if (Math.min(page1, page2) < 1) return true;
+  return false;
 }
 
 function makeScore(number) {
